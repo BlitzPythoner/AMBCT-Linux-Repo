ead0e58ba10ae65a3e7517bcd7a5ce24025fb571
Alex Blitz <alexblitz886@outlook.de>
2026-04-29 16:13:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing AMBCT..."
 
-wget -q https://raw.githubusercontent.com/BlitzPythoner/AMBCT-Repo/main/public.key -O /tmp/ambct.key
+wget -q https://raw.githubusercontent.com/BlitzPythoner/AMBCT-Linux-Repo/main/public.key -O /tmp/ambct.key
 gpg --dearmor < /tmp/ambct.key | sudo tee /usr/share/keyrings/ambct.gpg > /dev/null
 
 echo "deb [signed-by=/usr/share/keyrings/ambct.gpg] https://raw.githubusercontent.com/BlitzPythoner/AMBCT-Repo/main/ ./" | sudo tee /etc/apt/sources.list.d/ambct.list
